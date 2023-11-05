86efd45d8647c0d41b30b59300b194c8b47f4f9f
Beltzgetari <124810267+Beltzgetari@users.noreply.github.com>
2023-11-05 16:13:31 +0000

    localStorage

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express');
+let express = require('express');
 const path = require('path');
-const app = express();
+let app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.listen( 3000, function() {
+console.log("Zerbitzaria 3000 portuan entzuten");
+ })
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
@@ -8,9 +16,24 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.get("/", function(req, res) {
-    res.render('index', {'izenburua': 'EJS probatzen'})
+    res.render('index', {
+    'izenburua': 'EJS probatzen',
+    'bezeroak': bezeroak
+    })
     });
+ 
 
-app.get("/", (req, res) => {
-    res.render('index');
+let bezeroak =[];
+
+app.post("/bezeroa/new", function (req, res) {
+    bezeroak.push(req.body); // use req.body instead of req.params
+    console.log(req.body);
+
+    console.log(bezeroak);
+
+
+    res.render('index', {
+        'izenburua': 'EJS probatzen',
+        'bezeroak': bezeroak
+    });
 });
